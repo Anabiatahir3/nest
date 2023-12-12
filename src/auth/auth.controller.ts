b6348cccc6a61ad131678f93f4f7f2b500c37bec
Anabiatahir3 <anabiatahir365@gmail.com>
2023-12-12 14:03:48 +0500

    controller file

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param,Post,Body ,Patch,Delete,Query} from "@nestjs/common";
+
+@Controller('auth')
+
+export class UsersController{
+    //all our routes are defined here
+    //static routes such as "/hello" should be defined before dynamic routes such as "/:id"
+ @Get()
+    findAll(@Query("role")role?:"INTERN"| "ENGINEER"|"ADMIN"){
+        return []
+    }
+    @Get(':id')
+    findOne(@Param('id')id:string){
+    return {id}
+    }
+
+    @Post()
+    create(@Body()user:{}){
+        return user
+    }
+    @Patch(":id")
+    update(@Param("id") id :string, @Body()userUpdate:{}){
+        return {id,...userUpdate}
+    }
+    @Delete(":id")
+    delete(@Param("id") id :string){
+        return {id}
+    }
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './auth.controller';
+import { UsersService } from './auth.service';
+@Module({
+    controllers:[UsersController],
+    providers:[UsersService]
+})
+export class AuthModule{}
